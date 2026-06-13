a6f2aba516e91c7407f8f36edda2f1db6e82a8e7
KoenDuik-git <koend.duikersloot@gmail.com>
2026-06-13 10:33:50 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'dobbelstenen-app-cache-v1';
+const ASSETS = [
+  'dobbelstenen.html',
+  'manifest.json',
+  'icon.png'
+];
+
+// Sla de app-bestanden offline op bij installatie
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serveer de bestanden direct vanuit de cache wanneer offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
